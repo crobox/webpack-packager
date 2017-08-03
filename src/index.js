928ea04fc0a2c203e44e1f6a5570bd23c82e7965
Sjoerd Mulder <sjoerd@crobox.com>
2017-08-03 08:23:38 +0200

    Making port configurable

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const cluster = require('express-cluster');
 const os = require('os');
 const app = require ('./app');
 
-const PORT = process.env.NODE_ENV === 'production' ? 80 : 5500;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PACKAGER_PORT || 80) : 5500
 
 cluster(function (worker) {
   console.log('Hello from worker worker ' + worker.id);
-  return app.listen(PORT)
+  return app.listen(port)
 });
