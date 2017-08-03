828af9c2602e6fd0baab4539ee9415451b17d14b
Sjoerd Mulder <sjoerd@crobox.com>
2017-08-03 16:37:41 +0200

    Updating port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const cluster = require('express-cluster');
 const os = require('os');
 const app = require ('./app');
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PACKAGER_PORT || 80) : 5500
+const port = process.env.NODE_ENV === 'production' ? process.env.PACKAGER_PORT : 5500
 
 cluster(function (worker) {
   console.log('Hello from worker worker ' + worker.id);
